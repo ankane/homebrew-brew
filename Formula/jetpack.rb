9fa081eae5ef16cc5fdb0ca79d5471cee28e2496
Andrew <acekane1@gmail.com>
2018-06-14 04:03:07 -0700

    Updated Jetpack to 0.1.2

--- Formula/jetpack.rb
+++ Formula/jetpack.rb
@@ -1,13 +1,13 @@
 class Jetpack < Formula
   desc "Friendly package manager for R"
   homepage "https://github.com/ankane/jetpack"
-  url "https://github.com/ankane/jetpack/archive/v0.1.1.tar.gz"
-  sha256 "8747df17007b6a51cfb63a95d4530fa18833cb6b4281caabb0e1ef823e2dae96"
+  url "https://github.com/ankane/jetpack/archive/v0.1.2.tar.gz"
+  sha256 "cae0c86216e05517ed23f87bdc7db0a794a74154c5346077cea9987a4e8fd90b"
 
   depends_on "r"
 
   def install
-    bin.install "R/jetpack.R" => "jetpack"
+    bin.install "bin/jetpack" => "jetpack"
   end
 
   test do
