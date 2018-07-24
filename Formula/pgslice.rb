d81b9dbc52f67a7b50f94f08781c15954d5f3311
Andrew <acekane1@gmail.com>
2018-07-23 17:10:27 -0700

    Use url

--- Formula/pgslice.rb
+++ Formula/pgslice.rb
@@ -1,11 +1,11 @@
 class Pgslice < Formula
   desc "Postgres partitioning as easy as pie"
   homepage "https://github.com/ankane/pgslice"
-  url "https://github.com/ankane/pgslice.git",
-      :tag => "v0.4.1",
-      :revision => "223e2dd7cd5903fa6d817b3597a0a4027e86f8a3"
+  url "https://github.com/ankane/pgslice/archive/v0.4.2.tar.gz"
+  sha256 "e0f0bcc8bcd0e883b8bd37965f4528e6239b58afaa416cd549db96b1d3f06936"
 
   depends_on "libpq"
+  depends_on "ruby" if MacOS.version <= :sierra
 
   def install
     ENV["ARCHFLAGS"] = "-arch x86_64"
