2cafc7c7aa9bb0889ebdcb67bbb73a60bfe81552
Andrew <acekane1@gmail.com>
2018-08-16 19:48:34 -0700

    Updated pgslice version

--- Formula/pgslice.rb
+++ Formula/pgslice.rb
@@ -1,8 +1,8 @@
 class Pgslice < Formula
   desc "Postgres partitioning as easy as pie"
   homepage "https://github.com/ankane/pgslice"
-  url "https://github.com/ankane/pgslice/archive/v0.4.2.tar.gz"
-  sha256 "e0f0bcc8bcd0e883b8bd37965f4528e6239b58afaa416cd549db96b1d3f06936"
+  url "https://github.com/ankane/pgslice/archive/v0.4.3.tar.gz"
+  sha256 "b52804afeb5f13b40d09d0678b1d4e78d230337ea74d641639cf139458cefe68"
 
   depends_on "libpq"
   depends_on "ruby" if MacOS.version <= :sierra
