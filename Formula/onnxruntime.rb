aa3c67d0c900c6a0f0d6bdcc09b70a3ee36f8e3c
Andrew Kane <andrew@chartkick.com>
2019-08-26 22:27:10 -0700

    Better formula for ONNX Runtime

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -5,11 +5,25 @@ class Onnxruntime < Formula
   url "https://github.com/microsoft/onnxruntime/releases/download/v0.5.0/onnxruntime-osx-x64-0.5.0.tgz"
   sha256 "53993616d0928c746707f7c17d77b2c118d2404dfffb508d3fa2b20eaa7903fb"
 
+  bottle :unneeded
+
   def install
-    lib.install "lib/libonnxruntime.#{version}.dylib" => "libonnxruntime.dylib"
+    lib.install Dir["lib/*"]
+    include.install Dir["include/*"]
   end
 
   test do
-    system "true"
+    (testpath/"test.c").write <<~EOS
+      #include <onnxruntime_c_api.h>
+      #include <stdio.h>
+      int main()
+      {
+        printf("%s\\n", OrtGetVersionString());
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lonnxruntime",
+           testpath/"test.c", "-o", testpath/"test"
+    assert_equal version, shell_output("./test").strip
   end
 end
