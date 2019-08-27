6ec0e53eb2717b9db35f06a7d64ab9e5f4d2ef87
Andrew Kane <andrew@chartkick.com>
2019-08-26 22:39:06 -0700

    Removed extra line

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -1,7 +1,6 @@
 class Onnxruntime < Formula
   desc "Cross-platform, high performance scoring engine for ML models"
   homepage "https://github.com/microsoft/onnxruntime"
-
   url "https://github.com/microsoft/onnxruntime/releases/download/v0.5.0/onnxruntime-osx-x64-0.5.0.tgz"
   sha256 "53993616d0928c746707f7c17d77b2c118d2404dfffb508d3fa2b20eaa7903fb"
 
