ac61e9f4d554455d591fc3e551969dd48abf494a
Andrew Kane <andrew@ankane.org>
2022-07-27 18:48:35 -0700

    Updated dexter to 0.4.0

--- Formula/dexter.rb
+++ Formula/dexter.rb
@@ -1,25 +1,30 @@
 class Dexter < Formula
   desc "Automatic indexer for Postgres"
   homepage "https://github.com/ankane/dexter"
-  url "https://github.com/ankane/dexter/archive/v0.3.10.tar.gz"
-  sha256 "2228f84d9a853e92c12e993a102e0c5b76b09bb08bca21e7cca9f434e7c633e7"
+  url "https://github.com/ankane/dexter/archive/v0.4.0.tar.gz"
+  sha256 "e5bec6c6491500e3ff269c752bb339cd749a1c4f2c6f2c7c35f778c91cf228bb"
   license "MIT"
 
   depends_on "libpq"
 
+  resource "google-protobuf" do
+    url "https://rubygems.org/gems/google-protobuf-3.21.4.gem"
+    sha256 "3731263050ffc2b4fb1e3cdde13c34828f2ef057c1bd410e2ffc637b6ea8e795"
+  end
+
   resource "pg" do
-    url "https://rubygems.org/gems/pg-1.2.3.gem"
-    sha256 "f2e71e101eb7fc297222fa9a277a89a9686729a8dfa416d46408e696b5cfae8e"
+    url "https://rubygems.org/gems/pg-1.4.2.gem"
+    sha256 "faefc10f8b94b78f62f36923ada3a1412d89dbd4573afd7c91d0d55bf0195ed6"
   end
 
   resource "pg_query" do
-    url "https://rubygems.org/gems/pg_query-1.3.0.gem"
-    sha256 "0e1be8d88a8c0521b81f84d43efb8349ef1330861d4a353015b535018a1f34c5"
+    url "https://rubygems.org/gems/pg_query-2.1.3.gem"
+    sha256 "f3dd4b4c88c638eab48e9274f0dd88c584b60f8da58e3008b873192fe1e47001"
   end
 
   resource "slop" do
-    url "https://rubygems.org/gems/slop-4.9.1.gem"
-    sha256 "498de9c170ab15573a69aca92cc6199122be319e108de0f74a41cb26abdceb18"
+    url "https://rubygems.org/gems/slop-4.9.2.gem"
+    sha256 "5edcfaa749f3c978a7f8976520c8ec8aa174c3d5a1fcc52f05932eb7e442f2b5"
   end
 
   def install
