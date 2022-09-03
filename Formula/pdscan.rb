a4f1a6411cfd24c3203a865d4d89d347ef23bff3
Andrew Kane <andrew@ankane.org>
2022-09-03 16:06:44 -0700

    Added pdscan for Linux

--- Formula/pdscan.rb
+++ Formula/pdscan.rb
@@ -1,7 +1,10 @@
 class Pdscan < Formula
   desc "Scan your data stores for unencrypted personal data (PII)"
   homepage "https://github.com/ankane/pdscan"
-  if Hardware::CPU.arm?
+  if OS.linux?
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.2/pdscan_0.1.2_Linux_x86_64.zip"
+    sha256 "32e57362e2ed807edbc3b336b541d8ba70e78cfcf6f146aa576d2ec00a717495"
+  elsif Hardware::CPU.arm?
     url "https://github.com/ankane/pdscan/releases/download/v0.1.2/pdscan_0.1.2_Darwin_arm64.zip"
     sha256 "9dad8713e7edf8e0c11b6459eb14c1e176846038d9b2e3084f66d066c354c75e"
   else
