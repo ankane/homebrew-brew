78f14351bf1d632a73f041e5e43ea965b7fb0027
Andrew Kane <andrew@ankane.org>
2022-09-03 14:16:33 -0700

    Updated pgsync to 0.1.2

--- Formula/pdscan.rb
+++ Formula/pdscan.rb
@@ -1,9 +1,14 @@
 class Pdscan < Formula
   desc "Scan your data stores for unencrypted personal data (PII)"
   homepage "https://github.com/ankane/pdscan"
-  url "https://github.com/ankane/pdscan/releases/download/v0.1.1/pdscan_0.1.1_Darwin_x86_64.zip"
-  sha256 "5c33f4f39a2cfb93779f70a3d1296deb61572c6266d5c401fb5879bed672461b"
-  version "0.1.1"
+  if Hardware::CPU.arm?
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.2/pdscan_0.1.2_Darwin_arm64.zip"
+    sha256 "9dad8713e7edf8e0c11b6459eb14c1e176846038d9b2e3084f66d066c354c75e"
+  else
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.2/pdscan_0.1.2_Darwin_x86_64.zip"
+    sha256 "5cf19ec8282243bd268f0980aef51634395e9baf2553e50afda65ace8f33b4da"
+  end
+  version "0.1.2"
   license "MIT"
 
   def install
