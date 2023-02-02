6a43ee6aa350b1477b98a5a8fa9c2c1b8bd709c9
Andrew Kane <andrew@ankane.org>
2023-02-01 20:42:13 -0800

    Updated Random Cut Forest CLI to 0.1.1

--- Formula/rcf.rb
+++ Formula/rcf.rb
@@ -2,14 +2,14 @@ class Rcf < Formula
   desc "Command-line interface for Random Cut Forest anomaly detection"
   homepage "https://github.com/ankane/rcf-cli"
   if OS.linux?
-    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.0/rcf-cli-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f7603f22a15a35aaf5c43966e3be12481c48bf9a127f18a7f4748f7ca0792585"
+    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.1/rcf-cli-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "82123f1fb4f7ea56fd4edb997a61606ea8c6fb052ccbc41cc422a65da9f2d9be"
   elsif Hardware::CPU.arm?
-    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.0/rcf-cli-0.1.0-aarch64-apple-darwin.tar.gz"
-    sha256 "330cc475ffa2586ea7764f45cc340be50a6a99475161f84052d2cfd80bec4bd2"
+    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.1/rcf-cli-0.1.1-aarch64-apple-darwin.tar.gz"
+    sha256 "e3f4f2a0f4c87053eff53acd86f02745f52ec1aec13afee024ab2fe569d49d17"
   else
-    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.0/rcf-cli-0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "ef1b07668647e7357403d8fddf39c9be4f9730d630ceb160f994d759b70b4326"
+    url "https://github.com/ankane/rcf-cli/releases/download/v0.1.1/rcf-cli-0.1.1-x86_64-apple-darwin.tar.gz"
+    sha256 "fe6fbbd4e3a77d87fb963db2a3fc2ea721c4a44b3aa4533e8fa9495bd2d32976"
   end
   license "Apache-2.0"
 
