3ce89c5831b838201211ccbcc169c85b7ef7c99e
Andrew Kane <andrew@ankane.org>
2023-04-18 11:42:45 -0700

    Updated pdscan to 0.1.8

--- Formula/pdscan.rb
+++ Formula/pdscan.rb
@@ -2,14 +2,14 @@ class Pdscan < Formula
   desc "Scan your data stores for unencrypted personal data (PII)"
   homepage "https://github.com/ankane/pdscan"
   if OS.linux?
-    url "https://github.com/ankane/pdscan/releases/download/v0.1.7/pdscan-0.1.7-x86_64-linux.zip"
-    sha256 "068dd45cb1ac4654145070e6937a1d869381b7476227a3224bd613d56baff2a4"
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.8/pdscan-0.1.8-x86_64-linux.zip"
+    sha256 "d2b8b1556353de3cf83f0ab2528e5fe0d2fe17f1d11caf6fb110ec1b2585537f"
   elsif Hardware::CPU.arm?
-    url "https://github.com/ankane/pdscan/releases/download/v0.1.7/pdscan-0.1.7-arm64-darwin.zip"
-    sha256 "ca9aa25856b66ab62cf6744609a2ba15219fe6b035d9dc973c257334fe06848d"
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.8/pdscan-0.1.8-arm64-darwin.zip"
+    sha256 "c554359286decac446192cc0490baf56bb8232f60ad660e4e99776220fff225a"
   else
-    url "https://github.com/ankane/pdscan/releases/download/v0.1.7/pdscan-0.1.7-x86_64-darwin.zip"
-    sha256 "f04e4a38a5860fd398af257fe4673edcd35a10748a6905d4806ebee9c705dfa1"
+    url "https://github.com/ankane/pdscan/releases/download/v0.1.8/pdscan-0.1.8-x86_64-darwin.zip"
+    sha256 "0ebb02043a2be5d9d4bf868940b6981d6ffb27408c3e59a95948d5e129380f24"
   end
   license "MIT"
 
