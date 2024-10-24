f9304c43f5434b52b56e29c21c2a61bb262912da
Andrew Kane <andrew@ankane.org>
2024-10-23 23:31:17 -0700

    Updated librcf to 0.2.0

--- Formula/librcf.rb
+++ Formula/librcf.rb
@@ -2,14 +2,14 @@ class Librcf < Formula
   desc "Random Cut Forest anomaly detection for C/C++"
   homepage "https://github.com/ankane/librcf"
   if OS.linux?
-    url "https://github.com/ankane/librcf/releases/download/v0.1.1/librcf-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "66fc7c806be465e750e16eeb8392c6345082696b13145f84bb1f702a882f950f"
+    url "https://github.com/ankane/librcf/releases/download/v0.2.0/librcf-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "df88398e8dd950fbc503b382b280823713ca53931561e25b6d2f1c868bd37811"
   elsif Hardware::CPU.arm?
-    url "https://github.com/ankane/librcf/releases/download/v0.1.1/librcf-0.1.1-aarch64-apple-darwin.tar.gz"
-    sha256 "ae7adfd05a0f81c07b50d87f546dc542ba12acf2407d39e466e576c953e77523"
+    url "https://github.com/ankane/librcf/releases/download/v0.2.0/librcf-0.2.0-aarch64-apple-darwin.tar.gz"
+    sha256 "01fcb7b1330792223a5c7272ebe25a2ada2b081908331defe88242410ca5cddb"
   else
-    url "https://github.com/ankane/librcf/releases/download/v0.1.1/librcf-0.1.1-x86_64-apple-darwin.tar.gz"
-    sha256 "a41e04a3cb7074001f741abe37697e1e278364cf81e450188c68bd3ecdebe89c"
+    url "https://github.com/ankane/librcf/releases/download/v0.2.0/librcf-0.2.0-x86_64-apple-darwin.tar.gz"
+    sha256 "bfd2240cd40026791a6d9c44262f23a0a7439576c95d1d08b6b697a048aa16e9"
   end
   license "Apache-2.0"
 
